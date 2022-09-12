6f9f972bef13217a502b7bd604dd9081326b56ab
tmdgusya <tmdgusya@naver.com>
2022-09-12 16:51:14 +0900

    Solution20
    https://leetcode.com/problems/valid-parentheses/submissions/
    
    Stack And queue Solution

---
+++ solution20.py
@@ -0,0 +1,49 @@
+import unittest
+
+class Solution(unittest.TestCase):
+    def isValid(self, s: str) -> bool:
+        queue = list(s)
+        failed_stack: list[str] = []
+        while len(queue) > 0:
+            if len(failed_stack) == 0:
+                failed_stack.append(queue.pop(0))
+                continue
+
+            cur_parenthese = queue.pop(0)
+            prev_parenthese = failed_stack.pop()
+            if self.isCorrectWord(prev_parenthese, cur_parenthese):
+                continue
+            else:
+                failed_stack.append(prev_parenthese)
+                failed_stack.append(cur_parenthese)
+
+
+        return len(failed_stack) == 0
+
+    def isCorrectWord(self, target: str, curr: str) -> bool:
+        if target == '(':
+            if curr == ')':
+                return True
+            else:
+                return False
+        if target == '{':
+            if curr == '}':
+                return True
+            else:
+                return False
+        if target == '[':
+            if curr == ']':
+                return True
+            else:
+                return False
+        return False
+
+
+    def do(self):
+        self.assertTrue(self.isValid('()'))
+        self.assertFalse(self.isValid(')()('))
+        self.assertFalse(self.isValid('({[)}]'))
+        self.assertTrue(self.isValid('([])'))
+
+if __name__ == '__main__':
+    unittest.main()
